62ae885cff02a7297942b8419cc8294a75b749cd
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-10-17 09:27:54 -0500

    adds random id using crypto library

--- repos/users.js
+++ repos/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepo {
   constructor(filename) {
@@ -22,9 +23,20 @@ class UsersRepo {
   }
 
   async create(attrs) {
+    attrs.id = this.randomId();
     const records = await this.getAll();
     records.push(attrs);
-    await fs.promises.writeFile(this.filename, JSON.stringify(records));
+    await this.writeAll(records);
+  }
+  async writeAll(records) {
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString('hex');
   }
 }
 
