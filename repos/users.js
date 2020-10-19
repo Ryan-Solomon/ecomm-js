fe92b39bcbca35a7262ab2cb6861ce3f6c0585c4
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-10-18 20:05:51 -0500

    adds hashing

--- repos/users.js
+++ repos/users.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const util = require('util');
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepo {
   constructor(filename) {
@@ -24,6 +27,10 @@ class UsersRepo {
 
   async create(attrs) {
     attrs.id = this.randomId();
+
+    const salt = crypto.randomBytes(8).toString('hex');
+    const hashed = await crypto.scrypt(attrs.password, salt, 64);
+
     const records = await this.getAll();
     records.push(attrs);
     await this.writeAll(records);
